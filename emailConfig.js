90ecd2e3decc2f973bce9eb2d163873812119d08
Guilherme Lopes <guixlopesx2007@gmail.com>
2026-07-13 14:29:00 +0000

    Repository cleaning

--- emailConfig.js
+++
@@ -1,9 +0,0 @@
-PORT=3000
-MONGODB_URI=mongodb+srv://guixlopesx2007_db_user:AqNVOKjZdB4pzfYc@lopesfightacademy.7rnumae.mongodb.net/lfa?appName=LopesFightAcademy
-JWT_SECRET=lfa-2702-lopes
-
-EMAIL_SERVICE=gmail
-EMAIL_USER=guixlopesx2007@gmail.com
-EMAIL_PASSWORD=ttkq dstg ittm ogou
-EMAIL_FROM_NAME=LopesFightAcademy
-EMAIL_FROM_ADDRESS=guixlopesx2007@gmail.com
